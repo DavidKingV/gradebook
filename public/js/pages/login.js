0841640be8855b86d377ecd28ea52d5d21662c0b
DavidKingV <qet135ui@hotmail.com>
2024-07-09 16:31:36 -0600

    chore: Update login page functionality and styling

--- public/js/pages/login.js
+++ public/js/pages/login.js
@@ -3,9 +3,19 @@ import { loadingAlert, successAlert, errorAlert } from '../common/sweetAlert.js'
 
 var phpPath = "api/login.php";
 
-$(function() {
+function openInNewWindow(url) {
+    window.open(url, '_blank', 'width=800,height=600');
+}
 
-    $("#loginForm").submit(function(event) {
+$(function() {
+    $('[data-bs-toggle="tooltip"]').tooltip();
+
+    $("#openInNewWindow").on("click", function(event) {
+        event.preventDefault();
+        openInNewWindow("api/microsoftLogin.php");
+    });
+
+    $("#loginForm").on("submit", function(event) {
         event.preventDefault();
 
         let loginData = $(this).serialize();
@@ -18,7 +28,9 @@ $(function() {
                 if (data.success) {
                     successAlert(data.message)
                     .then((result) => {
-                        window.location.href = "resume.html";
+                        if (result.isConfirmed) {
+                            window.location.href = "inicio.php";
+                        }
                     });
                 } else {
                     errorAlert(data.message);
@@ -28,7 +40,20 @@ $(function() {
                 Swal.close();
                 errorAlert("Error en la petición AJAX");
             });
-
     });
+
+    window.addEventListener('message', function(event) {
+        if (event.data.accessToken) {
+            // Guardar el accessToken en la sesión o cookie si es necesario
+            // Redirigir a inicio.php
+            window.location.href = 'inicio.php?accessToken=' + event.data.accessToken;
+        } else if (event.data.error) {
+            // Manejar errores de autenticación
+            alert('Authentication failed');
+        } else{
+            // Manejar otros mensajes
+            alert('Unknown message');
+        }
+    }, false);
 
 });
