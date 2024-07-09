63c74c2c0edc59347d19fd2c259c762fc6aaf835
DavidKingV <qet135ui@hotmail.com>
2024-07-09 17:33:53 -0600

    coreccion de elementos del DOM

--- public/inicio.php
+++ public/inicio.php
@@ -132,11 +132,11 @@ var_dump($_SESSION);
                 <div class="row align-items-center justify-content-between flex-column flex-sm-row">
                     <div class="col-auto"><div class="small m-0">Copyright &copy; Your Website 2023</div></div>
                     <div class="col-auto">
-                        <a class="small" href="#!">Privacy</a>
+                        <a class="small" href="https://esmefis.edu.mx/aviso-de-privacidad/">Aviso de privacidad</a>
+                        <!--<span class="mx-1">&middot;</span>
+                        <a class="small" href="#!">Terminos y condiciones</a>-->
                         <span class="mx-1">&middot;</span>
-                        <a class="small" href="#!">Terms</a>
-                        <span class="mx-1">&middot;</span>
-                        <a class="small" href="#!">Contact</a>
+                        <a class="small" href="contacto.html">Contacto</a>
                     </div>
                 </div>
             </div>
