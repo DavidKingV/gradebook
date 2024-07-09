9793447f63a7ec34081561a6051e7304e37cbc05
DavidKingV <qet135ui@hotmail.com>
2024-07-09 16:34:59 -0600

    feat: Agregar controlador MicrosoftLogin y clases UserRepository, GetUserData, y gestión de sesión JWT
    Los cambios en el código introducen el controlador `MicrosoftLogin` para manejar la autenticación de Microsoft, las clases `UserRepository` y `GetUserData` para la recuperación de datos de usuario, y la gestión de sesión JWT para la funcionalidad de inicio de sesión. Estos cambios mejoran las capacidades de autenticación y gestión de sesión de la aplicación.

---
+++ public/api/microsoftLogin.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+use Esmefis\Gradebook\getEnv;
+use myPHPnotes\Microsoft\Auth;
+
+getEnv::cargar();
+
+$tenant = $_ENV['TENANT_ID'];
+$client_id = $_ENV['CLIENT_ID'];
+$client_secret = $_ENV['CLIENT_SECRET'];
+$callback = "http://localhost:8080/gradebookEsmefis/backend/controllers/MicrosoftLogin.php";
+$scopes = ["User.Read"];
+
+$microsoft = new Auth($tenant, $client_id,  $client_secret, $callback, $scopes);
+
+header("location: ". $microsoft->getAuthUrl()); //Redirecting to get access token
+
+?>
