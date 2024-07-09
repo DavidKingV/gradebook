bea0cd0538cbd91b7b1190ea750a8c7eed93667c
DavidKingV <qet135ui@hotmail.com>
2024-07-09 16:32:02 -0600

    feat: Add JWT session management for login
    The code changes introduce JWT session management for login functionality. The `SessionManager` class is updated to include methods for starting and ending sessions using JWT. The `startSession` method generates a JWT token with the user ID and name, sets it as a cookie, and starts the session. The `endSession` method destroys the session and clears all cookies. Additionally, a new `MicrosoftLoginControl` class is added to handle login data for Microsoft authentication. It includes methods for creating and checking login data using Microsoft access tokens.

--- backend/controllers/loginController.php
+++ backend/controllers/loginController.php
@@ -3,7 +3,52 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 
 use Esmefis\Gradebook\getEnv;
 use Esmefis\Gradebook\DBConnection;
-use Esmefis\Gradebook\verifyAuth;
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+
+getEnv::cargar();
+
+class SessionManager {
+    public static function startSession($uID, $uName) {
+        session_set_cookie_params($_ENV['LIFE_TIME']);
+        session_start();
+
+        $_SESSION['uID'] = $uID;
+
+        $payLoad = [
+            'uID' => $uID,
+            'uName' => $uName
+        ];
+
+        $jwt = JWT::encode($payLoad, $_ENV['SECRET_KEY'], 'HS256');
+        setcookie('LoSessionToken', $jwt, time() + $_ENV['LIFE_TIME'], "/", "", true, true);
+
+        return true;
+    }
+
+    public static function endSession() {
+        session_start();
+
+        if (!isset($_SESSION["adnanhussainturki/microsoft"])) {
+            session_unset();
+            session_destroy();  
+
+            // Eliminar todas las cookies
+            foreach ($_COOKIE as $key => $value) {
+                setcookie($key, '', time() - 3600, '/');
+            }
+
+            return array("success" => true, "message" => "Sesión cerrada");
+        }else{
+            session_unset();
+            session_destroy();  
+
+            return array("success" => true, "microsoftLogout" => true, "message" => "Cerrando sesión de Microsoft");
+        }
+
+        
+    }
+}
 
 class LoginControl{
 
@@ -16,7 +61,7 @@ class LoginControl{
     public function login($userData){
         parse_str($userData, $userData);
 
-        $user = $userData['user'];
+        $user = filter_var($userData['user'], FILTER_SANITIZE_STRING);
         $password = $userData['password'];
 
         $query = "SELECT id, password, hashed_password FROM login_students WHERE user = ?";
@@ -40,11 +85,18 @@ class LoginControl{
                 $stmt_update = $this->connection->prepare($sql_update);
                 $stmt_update->bind_param("si", $new_hashed_password, $row['id']);
                 $stmt_update->execute();
-                $stmt_update->close();
                 
-                return array("success" => true, "message" => "Inicio de sesión exitoso (y contraseña actualizada)", "uID" => $row['id']);
+                if ($stmt_update->affected_rows <= 0) {
+                    return array("success" => false, "message" => "Error al actualizar la contraseña, por favor intente de nuevo más tarde");
+                }else{
+                    $stmt_update->close();
+                    SessionManager::startSession($row['id'], $user);
+                    return array("success" => true, "message" => "Inicio de sesión exitoso (y contraseña actualizada)", "uID" => $row['id']);
+                }
+                
             } elseif ($stored_hashed_password !== null && password_verify($password, $stored_hashed_password)) {
                 // La contraseña está hashada en la base de datos y coincide con la contraseña proporcionada
+                SessionManager::startSession($row['id'], $user);
                 return array("success" => true, "message" => "Inicio de sesión exitoso", "uID" => $row['id']);
             } else {
                 // La contraseña no coincide
@@ -56,4 +108,51 @@ class LoginControl{
         }
     }
 
+}
+
+class MicrosoftLoginControl{
+    
+    private $connection;
+    
+    public function __construct(DBConnection $dbConnection) {
+        $this->connection = $dbConnection->getConnection();
+    }
+
+    public function createLoginData($accessToken, $userName, $userEmail){
+        $query = "INSERT INTO microsoft_users (microsoft_access_token, microsoft_user_name, microsoft_user_email) VALUES (?, ?, ?)";
+        $stmt = $this->connection->prepare($query);
+        $stmt->bind_param('sss', $accessToken, $userName, $userEmail);
+        $stmt->execute();
+    
+        if ($stmt->execute()) {
+            if ($stmt->affected_rows > 0) {
+                $stmt->close();
+                return true;
+            }
+        }
+
+        $stmt->close();
+        return false;
+    }
+    
+    public function checkLoginData($accessToken, $userName = null, $userEmail = null){
+        $query = "SELECT id FROM microsoft_users WHERE microsoft_access_token = ?";
+        $stmt = $this->connection->prepare($query);
+        $stmt->bind_param('s', $accessToken);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        
+        if ($result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+            $stmt->close();
+            return array("success" => true);
+        } else {
+            $stmt->close();
+            if ($this->createLoginData($accessToken, $userName, $userEmail)) {
+                return array("success" => true, "message" => "Datos creados y guardados correctamente");
+            } else {
+                return array("success" => false, "message" => "No se pudieron guardar los datos");
+            }
+        }
+    }
 }
