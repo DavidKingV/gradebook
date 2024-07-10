5e3d1405835c96932d4ddc69052f67fceb369ae7
DavidKingV <qet135ui@hotmail.com>
2024-07-10 13:25:04 -0600

    coreccion en los required

--- backend/controllers/MicrosoftLogin.php
+++ backend/controllers/MicrosoftLogin.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 require_once __DIR__ . '/../../vendor/autoload.php';
-require_once __DIR__ . '/loginController.php';
+require_once __DIR__ . '/SessionController.php';
 
 use Esmefis\Gradebook\DBConnection;
 use myPHPnotes\Microsoft\Models\User;
