d462129bd5eb374df2f4e6de6cb600ca58835356
DavidKingV <qet135ui@hotmail.com>
2024-08-02 17:06:30 -0600

    bug fixed

--- backend/controllers/MicrosoftLogin.php
+++ backend/controllers/MicrosoftLogin.php
@@ -4,14 +4,14 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 require_once __DIR__ . '/SessionController.php';
 
 use Esmefis\Gradebook\DBConnection;
-use Esmefis\Gradebook\GetUserData;
+use Esmefis\Gradebook\getUserData;
 use myPHPnotes\Microsoft\Models\User;
 use myPHPnotes\Microsoft\Auth;
 use myPHPnotes\Microsoft\Handlers\Session;
 
 $dbConnection = new DBConnection();
 $connection = $dbConnection->getConnection();
-$userData = new getUserData($connection);
+$userData = new GetUserData($connection);
 
 $microsoft = new Auth(Session::get("tenant_id"),Session::get("client_id"),  Session::get("client_secret"), Session::get("redirect_uri"), Session::get("scopes"));
 
